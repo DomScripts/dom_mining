4452ba44d8d002dba0ba51ed764fffb4ee2f5d12
DomScripts <59152632+DomScripts@users.noreply.github.com>
2023-02-10 16:10:25 -0500

    Add files via upload

---
+++ _installation/items.lua
@@ -0,0 +1,64 @@
+['jackhammer'] = {
+    label = 'Jack Hammer',
+    weight = 10000,
+    stack = false,
+    close = true,
+},
+['pickaxe'] = {
+    label = 'Pickaxe',
+    weight = 2500,
+    stack = false,
+    close = true,
+},
+['shovel'] = {
+    label = 'Shovel',
+    weight = 1500,
+    stack = false,
+    close = true,
+},
+
+['full_bucket'] = {
+    label = 'Full Bucket',
+    weight = 1000,
+    stack = true,
+    close = false,
+},
+['empty_bucket'] = {
+    label = 'Empty Bucket',
+    weight =  500,
+    stack = true,
+    close = false,
+},
+['gem_rock'] = {
+    label = 'Gem Rock',
+    weight = 1,
+    stack = true,
+    close = false,
+},
+['ruby'] = {
+    label = 'Ruby',
+    weight = 0.1,
+    stack = true,
+    close = false,
+},
+['sapphire'] = {
+    label = 'Sapphire',
+    weight = 0.1,
+    stack = true,
+    close = false,
+},
+['emerald'] = {
+    label = 'Emerald',
+    weight = 0.1,
+    stack = true,
+    close = false,
+},
+
+-- By deault ox_inventory has a diamond item but if you removed it include this
+['diamond'] = {
+    label = 'Diamond',
+    weight = 1,
+    stack = true,
+    close = true,
+    description = nil
+},
---
+++ _installation/shops.lua
@@ -0,0 +1,13 @@
+Mining = {
+    name = 'Mining Shop',
+    inventory = {
+        {name = 'jackhammer', price =  5000},
+        {name = 'pickaxe', price = 2500},
+        {name = 'shovel', price = 1000},
+        {name = 'empty_bucket', price =  10}
+    }, locations = {
+        vec3(2707.3708, 2776.7751, 36.8780)
+    }, targets = {
+        {loc = vec3(2707.3708, 2776.7751, 37.8780), length = 2.0, width = 2.0, heading = 360.0, minZ = 28.2, maxZ = 28.6, distance = 2.0 }
+    }
+}
